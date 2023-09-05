56ca9e9aebcf8944dcc4092ca1617673afdf8d9b
YFisch <yfisch381@gmail.com>
2023-09-05 13:17:39 -0400

    Fixed Login

---
+++ RecipeApps/RecipeWinForms/frmLogin.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace RecipeWinForms
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tblMain = new TableLayoutPanel();
+            lblUserName = new Label();
+            lblPassword = new Label();
+            txtPassword = new TextBox();
+            txtUserId = new TextBox();
+            tblButtons = new TableLayoutPanel();
+            btnLogin = new Button();
+            btnCancel = new Button();
+            tblMain.SuspendLayout();
+            tblButtons.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tblMain
+            // 
+            tblMain.ColumnCount = 2;
+            tblMain.ColumnStyles.Add(new ColumnStyle());
+            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tblMain.Controls.Add(lblUserName, 0, 0);
+            tblMain.Controls.Add(lblPassword, 0, 1);
+            tblMain.Controls.Add(txtPassword, 1, 1);
+            tblMain.Controls.Add(txtUserId, 1, 0);
+            tblMain.Controls.Add(tblButtons, 1, 2);
+            tblMain.Dock = DockStyle.Fill;
+            tblMain.Location = new Point(0, 0);
+            tblMain.Margin = new Padding(4, 4, 4, 4);
+            tblMain.Name = "tblMain";
+            tblMain.RowCount = 4;
+            tblMain.RowStyles.Add(new RowStyle());
+            tblMain.RowStyles.Add(new RowStyle());
+            tblMain.RowStyles.Add(new RowStyle());
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+            tblMain.Size = new Size(744, 225);
+            tblMain.TabIndex = 0;
+            // 
+            // lblUserName
+            // 
+            lblUserName.AutoSize = true;
+            lblUserName.Location = new Point(4, 0);
+            lblUserName.Margin = new Padding(4, 0, 4, 0);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(88, 21);
+            lblUserName.TabIndex = 0;
+            lblUserName.Text = "User Name";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(4, 35);
+            lblPassword.Margin = new Padding(4, 0, 4, 0);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(76, 21);
+            lblPassword.TabIndex = 1;
+            lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Dock = DockStyle.Fill;
+            txtPassword.Location = new Point(99, 38);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(642, 29);
+            txtPassword.TabIndex = 2;
+            // 
+            // txtUserId
+            // 
+            txtUserId.Dock = DockStyle.Fill;
+            txtUserId.Location = new Point(99, 3);
+            txtUserId.Name = "txtUserId";
+            txtUserId.Size = new Size(642, 29);
+            txtUserId.TabIndex = 3;
+            // 
+            // tblButtons
+            // 
+            tblButtons.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            tblButtons.ColumnCount = 2;
+            tblButtons.ColumnStyles.Add(new ColumnStyle());
+            tblButtons.ColumnStyles.Add(new ColumnStyle());
+            tblButtons.Controls.Add(btnLogin, 0, 0);
+            tblButtons.Controls.Add(btnCancel, 1, 0);
+            tblButtons.Location = new Point(541, 73);
+            tblButtons.Name = "tblButtons";
+            tblButtons.RowCount = 1;
+            tblButtons.RowStyles.Add(new RowStyle());
+            tblButtons.Size = new Size(200, 42);
+            tblButtons.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Anchor = AnchorStyles.None;
+            btnLogin.AutoSize = true;
+            btnLogin.Location = new Point(3, 5);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 31);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "Ok";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Anchor = AnchorStyles.None;
+            btnCancel.AutoSize = true;
+            btnCancel.Location = new Point(103, 5);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 31);
+            btnCancel.TabIndex = 1;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(744, 225);
+            Controls.Add(tblMain);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "frmLogin";
+            Text = "HeartyHearth Login";
+            tblMain.ResumeLayout(false);
+            tblMain.PerformLayout();
+            tblButtons.ResumeLayout(false);
+            tblButtons.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tblMain;
+        private Label lblUserName;
+        private Label lblPassword;
+        private TextBox txtPassword;
+        private TextBox txtUserId;
+        private TableLayoutPanel tblButtons;
+        private Button btnLogin;
+        private Button btnCancel;
+    }
+}
